77aa7b5dbf541a72b1b8329b57a5f34584113bdd
mishkat01 <124544908+mishkat01@users.noreply.github.com>
2023-12-10 20:44:34 +0600

    style login reg

--- user/src/pages/LoginPage.jsx
+++ user/src/pages/LoginPage.jsx
@@ -8,67 +8,54 @@ function LoginPage() {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
   const navigate = useNavigate();
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     // console.log(email, password);
     http
       .post("/auth/login", { username: username, password: password })
       .then((response) => {
-        console.log('hello');
-
         const token = response.data.access_token;
         const user = response.data.user;
         setToken(token);
         setUser(user);
-      
-          if (response.data.user.role === "admin") {
+
+        if (response.data.user.role === "admin") {
           navigate("/adminDashboard");
         } else if (response.data.user.role === "vendor") {
           navigate("/vendor");
-        } else if (response.data.user.role === "user"){
+        } else if (response.data.user.role === "user") {
           navigate("/dashboard");
         }
       })
-      .catch(function (error){
+      .catch(function (error) {
         alert(error.response.data.errors);
-      })
+      });
+    e.preventDefault();
   };
   return (
     <>
       <Nav />
-      <form>
-        <div style={{ margin: "100px 500px" }}>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              onChange={(e) => setUsername(e.target.value)}
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
-          <br />
+      <div className="custom_container">
+        <form onSubmit={handleSubmit}>
+          <label>Username</label>
+          <input
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            id="exampleInputEmail1"
+            placeholder="Username"
+          />
 
-          <button
-            type="button"
-            onClick={handleSubmit}
-            className="btn btn-primary"
-          >
+          <label>Password</label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+
+          <button className="login_button" type="submit">
             Login
           </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 }
--- user/src/pages/RegisterPage.jsx
+++ user/src/pages/RegisterPage.jsx
@@ -27,52 +27,44 @@ function RegisterPage() {
   return (
     <>
       <Nav />
-      <form>
-        <div style={{ margin: "100px 500px" }}>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Username</label>
-            <input
-              onChange={(e) => setUsername(e.target.value)}
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter username"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Phone</label>
-            <input
-              onChange={(e) => setPhone(e.target.value)}
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-          </div>
-          <br />
+      <div className="custom_container">
+        <form>
+          <label htmlFor="exampleInputEmail1">Username</label>
+          <input
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            className=""
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter username"
+          />
+
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            className=""
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            className=""
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <label htmlFor="exampleInputEmail1">Phone</label>
+          <input
+            onChange={(e) => setPhone(e.target.value)}
+            type="text"
+            className=""
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter Phone"
+          />
           <label for="cars">Register As:</label>
 
           <select id="cars" onChange={(e) => setRole(e.target.value)}>
@@ -81,18 +73,12 @@ function RegisterPage() {
               User
             </option>
           </select>
-          <br />
-          <br />
 
-          <button
-            type="button"
-            onClick={handleSubmit}
-            className="btn btn-primary"
-          >
+          <button type="button" onClick={handleSubmit} className="login_button">
             Register
           </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 }
