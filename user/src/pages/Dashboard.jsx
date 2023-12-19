7d236ef82a2de56646281b7bedfbf1ab28aea376
mishkat01 <124544908+mishkat01@users.noreply.github.com>
2023-12-20 02:28:02 +0600

    usrdashboard

--- user/src/pages/Dashboard.jsx
+++ user/src/pages/Dashboard.jsx
@@ -1,36 +1,21 @@
 import Nav from "../components/Nav";
-import { useEffect } from "react";
+
 import AuthUser from "../components/AuthUser";
 import { Navigate } from "react-router-dom";
 function Dashboard() {
-  const { user, http } = AuthUser();
-  const { token, logout, getToken } = AuthUser();
-  http.get("auth/me").then((res) => {
-    console.log(res.data);
-  });
-  useEffect(() => {
-    const checkToken = () => {
-      const currentToken = getToken();
-      if (currentToken !== token) {
-        setToken(currentToken);
-        if (!currentToken) {
-          return <Navigate to="/login" />;
-        }
-      }
-    };
+  const { token, logout, getToken, user, http } = AuthUser();
+  // http.get("auth/me").then((res) => {
+  //   console.log(res.data);
+  // });
 
-    window.addEventListener("storage", checkToken);
-
-    return () => {
-      window.removeEventListener("storage", checkToken);
-    };
-  }, [token, getToken]);
+  if (!getToken()) {
+    return <Navigate to="/login" />;
+  }
   const handleLogout = () => {
     if (token != undefined) {
       logout();
     }
   };
-  console.log(token);
 
   return (
     <>
